73336844264e100d509664b4aa40932d51c57f5f
onlypanther <106186752+onlypanther@users.noreply.github.com>
2023-09-22 23:44:11 -0500

    Update crear_carrito.py

--- crear_carrito.py
+++ crear_carrito.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     carro.agregar_usuario(usuario1)
 
      cantina1= cantina("cantina 1", "Dirección 1", "Ciudad 1")
-    carro.agregar_bar(bar1)
+    carro.agregar_bar(cantina1)
 
-    # Muestra el contenido del carro
+ 
     carro.mostrar_carro()
